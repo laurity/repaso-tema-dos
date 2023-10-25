e0e128e40388b75d8a888d19fbd2d8ee9e8d70c6
Laura López <99351746+laurity@users.noreply.github.com>
2023-10-25 10:13:27 +0200

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,87 @@
  * Iniciar Sesion
  * Operaciones Cajero
  */
+/**
+ * Variables:
+ * saldo
+ * pin-correcto
+ * intentos restantes
+ */
+
+let saldo = 1000; //estarian en una base de datos
+
+const PIN_CORRECTO = "1234"; //lo mismo que la de arriba.
+
+let intentosRestantes = 3;
+
+function mostrarSaldo() {
+  console.log(`Su saldo actual es ${saldo.toFixed(2)} €`);
+}
+
+function depositarSaldo() {
+  const deposito = parseFloat(prompt("Ingrese la cantidad a depositar"));
+
+  if (isNaN (deposito) || deposito <= 0) {
+
+    console.log(`Cantidad inválida. Intente de nuevo`);
+
+  } else {
+    
+    saldo += deposito;
+
+    console.log(`Saldo depositado ${deposito.toFixed(2)}`);
+
+    mostrarSaldo();
+  }
+}
+
+function retirar() {
+  const retiro = parseFloat(prompt("Ingrese la cantidad a retirar"));
+  if (isNaN (retiro) || retiro <= 0 || retiro > saldo) {
+    console.log(`Cantidad inválida. Intente de nuevo`);
+  }
+  else{
+    saldo -= retiro;
+    console.log(`Saldo retirado ${retiro.toFixed(2)}`);
+    mostrarSaldo();
+  }
+}
+
+function transferir() {
+  const monto = parseFloat(prompt("Ingrese la cantidad a transferir"));
+  if(isNaN(monto) || monto <= 0 || monto > saldo) {
+    console.log(`Cantidad inválida. Intente de nuevo`);
+}
+else{
+  const cuentaDestino = prompt("Ingrese la cuenta destino"); 
+  console.log(`Se han transferido €${monto.toFixed(2)} a la cuenta ${cuentaDestino}`);
+  saldo -= monto;
+  mostrarSaldo();
+}
+}
+
+
+function isValidStructureIBAN(cuentaAValidar){
+  return /[a-zA-Z]{2}[0-9]{20}$/g.test(cuentaAValidar);
+}// Fin de la función isValidStructureIBAN.
+
+function iniciarSesion() {
+  let pin = prompt("Ingrese su pin");
+  while (pin!== PIN_CORRECTO  && intentosRestantes > 1) {
+    intentosRestantes--;
+    console.log(`PIN incorrecto. Intentos restantes: ${intentosRestantes}`);
+    pin = prompt("Ingrese su pin");
+  }
+  if(pin === PIN_CORRECTO) {
+    console.log(`PIN correcto`);
+    mostrarSaldo();
+    operacionesCajero();
+}
+else{
+  console.log(`PIN incorrecto. El cajero se ha bloquado.`);
+}
+
+function operacionesCajero() {
+  //esto seria el menu.
+}
+}
